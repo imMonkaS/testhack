5a6b8f887e66fa38c1541ee2006b20a8f5fbcedb
MonkaS <danchik646@gmail.com>
2024-09-10 18:32:31 +0300

    Скелет проекта

---
+++ docs/index.md
@@ -0,0 +1,6 @@
+# Запуск приложения
+## Конфигурация
+Задайте настройки приложения
+```bash
+cp src/main/resources/application.properties.tmpl src/main/resources/application.properties
+```
---
+++ src/main/java/com/test/testhack/TesthackApplication.java
@@ -0,0 +1,13 @@
+package com.test.testhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesthackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesthackApplication.class, args);
+	}
+
+}
